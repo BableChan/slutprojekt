c6574d05b11a054e51ac2908df7a4098fe38417a
BableChan <rasmus.widang@gmail.com>
2026-03-27 10:37:12 +0100

    ajfaeiujgpaenrgpiwae

---
+++ ConsoleApp1/Character.cs
@@ -0,0 +1,7 @@
+public class Character(string name, int hitPoints, int basicAttackDamage, int gimmickAttack)
+{
+    public string Name { get; } = name;
+    public int HitPoints { get; set; } = hitPoints;
+    public int BasicAttackDamage { get; set; } = basicAttackDamage;
+    public int GimmickAttack { get; set; } = gimmickAttack;
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+Character Phaquirr = new(
+    name: "Phaquirr",
+    hitPoints: 500,
+    basicAttackDamage: 70,
+    gimmickAttack: 70
+);
+
+
+Character Aryas = new(
+    name: "Arya",
+    hitPoints: 400,
+    basicAttackDamage: Random.Shared.Next(65, 100),
+    gimmickAttack: 20
+);
+
+
+CoinFlip();
+Rounds(Phaquirr, Aryas);
+
+
+
+
+// var characterOrderAssignment
+
+
+
+
+
+
+
+
+
+static int CoinFlip()
+{
+    int coinFlipSleepAnimation = 30;
+    Console.WriteLine("Flip a coin tetermine who starts");
+    Console.ReadLine();
+    for (int i = 0; i < coinFlipSleepAnimation; i++)
+    {
+        Thread.Sleep(10 * i);
+        Console.Clear();
+        int coinFlip = Random.Shared.Next(1, 3);
+        string coinFlipName = "tails";
+        if(coinFlip == 1)
+        {
+            coinFlipName = "Heads";
+        }
+        Console.WriteLine(coinFlipName);
+    }
+
+    var coinFlipResult = Random.Shared.Next(1, 3);
+    Console.Clear();
+    Console.WriteLine(coinFlipResult);
+    Thread.Sleep(1000);
+    if (coinFlipResult == 1)
+    {
+
+    }
+    Console.WriteLine($"The coin has spoken. Player{coinFlipResult} starts.");
+    Thread.Sleep(1000);
+    Console.WriteLine("press enter to continue...");
+    Console.ReadLine();
+    Console.Clear();
+    return coinFlipResult;
+}
+
+static void Rounds(Character Phaquirr, Character Aryas)
+{
+    Console.WriteLine(Phaquirr.Name);
+    Console.ReadLine();
+}
